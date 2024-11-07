ea1ced27daddd1bd4283a7d5e5e624498fb571b8
37952 <edwardfan3737@gmail.com>
2024-11-06 16:13:47 -0800

    V

--- main.ts
+++ main.ts
@@ -1,1 +1,41 @@
-
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    let mySprite: Sprite = null
+    tiles.placeOnRandomTile(mySprite, sprites.builtin.forestTiles0)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (money_jump_count <= 1) {
+        Money.vy = -150
+        money_jump_count += 1
+    }
+})
+let money_jump_count = 0
+let Money: Sprite = null
+scene.setBackgroundColor(9)
+let GRAVITY = 370
+tiles.setCurrentTilemap(tilemap`level2`)
+Money = sprites.create(img`
+    . . . . 8 8 8 8 8 . . . . . . . 
+    . . . 8 2 2 2 2 2 8 . . . . . . 
+    . . 8 d d d d 2 2 2 8 . . . . . 
+    . 8 d f d d f d 2 2 8 8 . . . . 
+    . 8 d f d d f d 2 2 d d 8 . . . 
+    8 d 2 2 d d d d 2 2 b d 8 . . . 
+    8 d d d d 8 d d 2 2 b d 8 . 8 8 
+    8 8 8 8 8 d d d 2 2 8 8 . 8 2 8 
+    . 8 d d d d d 2 2 8 8 . . 8 2 8 
+    . . 8 8 8 8 8 2 2 2 2 8 . 8 2 8 
+    . . . . 8 2 2 2 2 2 2 2 8 8 2 8 
+    . . . 8 2 f f 2 f 2 2 2 2 8 8 . 
+    . . . 8 2 f f 2 f 2 2 2 2 8 . . 
+    . . . 8 d b f d b f f 2 8 . . . 
+    . . . 8 d d c d d b b d 8 . . . 
+    . . . . 8 8 8 8 8 8 8 8 8 . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(Money, 100, 0)
+Money.ay = GRAVITY
+money_jump_count = 2
+game.onUpdate(function () {
+    if (Money.isHittingTile(CollisionDirection.Bottom)) {
+        money_jump_count = 0
+    }
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,60 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`0a000a0000030000000000000300000000000000000000000001010100000101010000000000000000000000000000000000000000000000010101010101000000010000000000000100000000000000000000000000000000000000000002020202020202020202`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. 2 2 2 . . 2 2 2 . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . 2 2 2 2 2 2 . . 
+. 2 . . . . . . 2 . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,sprites.builtin.forestTiles0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
